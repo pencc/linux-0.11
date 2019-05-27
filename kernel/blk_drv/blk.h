adbc239a300b6173f821d0f417524b4401f5feca
pencc <yxpeng@linx-info.com>
2019-05-27 10:52:29 +0800

    添加了blk.h的注释

--- kernel/blk_drv/blk.h
+++ kernel/blk_drv/blk.h
@@ -57,37 +57,37 @@ extern struct task_struct * wait_for_request;		// 等待空闲请求项的进程
 
 // 在块设备驱动程序(如hd.c)包含此头文件时，必须先定义驱动程序处理设备的主设备号
 // 这样下面就能为包含本文件的驱动程序给出正确的宏定义。
-#ifdef MAJOR_NR
+#ifdef MAJOR_NR			// 主设备号
 
 /*
  * Add entries as needed. Currently the only block devices
- * supported are hard-disks and floppies.
+ * supported are hard-disks、 floppies and ramdisk.
  */
 
-#if (MAJOR_NR == 1)
+#if (MAJOR_NR == 1)							// RAM盘的主设备号是1
 /* ram disk */
-#define DEVICE_NAME "ramdisk"
-#define DEVICE_REQUEST do_rd_request
-#define DEVICE_NR(device) ((device) & 7)
-#define DEVICE_ON(device) 
-#define DEVICE_OFF(device)
+#define DEVICE_NAME "ramdisk"				// 设备名称ramdisk
+#define DEVICE_REQUEST do_rd_request		// 设备请求函数do_rd_request()
+#define DEVICE_NR(device) ((device) & 7)	// 设备号0-7
+#define DEVICE_ON(device) 					// 开启设备，虚拟盘无须开启和关闭
+#define DEVICE_OFF(device)					// 关闭设备
 
-#elif (MAJOR_NR == 2)
+#elif (MAJOR_NR == 2)						// 软驱的主设备号是2
 /* floppy */
 #define DEVICE_NAME "floppy"
-#define DEVICE_INTR do_floppy
-#define DEVICE_REQUEST do_fd_request
-#define DEVICE_NR(device) ((device) & 3)
-#define DEVICE_ON(device) floppy_on(DEVICE_NR(device))
-#define DEVICE_OFF(device) floppy_off(DEVICE_NR(device))
+#define DEVICE_INTR do_floppy				// 设备中断处理程序do_floppy()
+#define DEVICE_REQUEST do_fd_request		// 设备请求函数do_fd_request()
+#define DEVICE_NR(device) ((device) & 3)	// 设备号0-3
+#define DEVICE_ON(device) floppy_on(DEVICE_NR(device))		// 开启设备宏
+#define DEVICE_OFF(device) floppy_off(DEVICE_NR(device))	// 关闭设备宏
 
-#elif (MAJOR_NR == 3)
+#elif (MAJOR_NR == 3)						// 硬盘主设备号是3
 /* harddisk */
 #define DEVICE_NAME "harddisk"
-#define DEVICE_INTR do_hd
-#define DEVICE_REQUEST do_hd_request
-#define DEVICE_NR(device) (MINOR(device)/5)
-#define DEVICE_ON(device)
+#define DEVICE_INTR do_hd					// 设备中断处理程序do_hd()
+#define DEVICE_REQUEST do_hd_request		// 设备请求函数do_hd_request()
+#define DEVICE_NR(device) (MINOR(device)/5)	// 设备号(0--1)，每个硬盘可有4个分区
+#define DEVICE_ON(device)					// 硬盘一直在工作，无须开启和关闭
 #define DEVICE_OFF(device)
 
 #elif
@@ -96,14 +96,17 @@ extern struct task_struct * wait_for_request;		// 等待空闲请求项的进程
 
 #endif
 
-#define CURRENT (blk_dev[MAJOR_NR].current_request)
-#define CURRENT_DEV DEVICE_NR(CURRENT->dev)
+#define CURRENT (blk_dev[MAJOR_NR].current_request)		// 指定主设备号的当前请求项指针
+#define CURRENT_DEV DEVICE_NR(CURRENT->dev)				// 当前请求项CURRENT中的设备号
 
 #ifdef DEVICE_INTR
 void (*DEVICE_INTR)(void) = NULL;
 #endif
 static void (DEVICE_REQUEST)(void);
 
+// 解锁指定的缓冲区
+// 如果指定的缓冲区bh并没有被上锁，则显示警告信息。否则将该缓冲区解锁，并等待唤醒
+// 等待该缓冲区的进程。参数是缓冲区头指针
 static inline void unlock_buffer(struct buffer_head * bh)
 {
 	if (!bh->b_lock)
@@ -114,30 +117,29 @@ static inline void unlock_buffer(struct buffer_head * bh)
 
 static inline void end_request(int uptodate)
 {
-	DEVICE_OFF(CURRENT->dev);
-	if (CURRENT->bh) {
-		CURRENT->bh->b_uptodate = uptodate;
-		unlock_buffer(CURRENT->bh);
+	DEVICE_OFF(CURRENT->dev);				// 关闭设备
+	if (CURRENT->bh) {						// CURRENT为当前请求结构项指针
+		CURRENT->bh->b_uptodate = uptodate;	// 置更新标志
+		unlock_buffer(CURRENT->bh);			// 解锁缓冲区
 	}
-	if (!uptodate) {
+	if (!uptodate) {						// 若更新标志为0则显示出错信息
 		printk(DEVICE_NAME " I/O error\n\r");
 		printk("dev %04x, block %d\n\r",CURRENT->dev,
 			CURRENT->bh->b_blocknr);
 	}
-	wake_up(&CURRENT->waiting);
-	wake_up(&wait_for_request);
-	CURRENT->dev = -1;
-	CURRENT = CURRENT->next;
+	wake_up(&CURRENT->waiting);				// 唤醒等待该请求项的进程
+	CURRENT->dev = -1;						// 释放该请求项
+	CURRENT = CURRENT->next;				// 从请求链表中删除该请求项，并且当前指针指向下一请求项目
 }
 
 #define INIT_REQUEST \
 repeat: \
-	if (!CURRENT) \
+	if (!CURRENT) \							// 如果当前请求结构指针为null则返回
 		return; \
-	if (MAJOR(CURRENT->dev) != MAJOR_NR) \
+	if (MAJOR(CURRENT->dev) != MAJOR_NR) \	// 如果当前设备的主设备号不正确则死机
 		panic(DEVICE_NAME ": request list destroyed"); \
 	if (CURRENT->bh) { \
-		if (!CURRENT->bh->b_lock) \
+		if (!CURRENT->bh->b_lock) \			// 如果请求项的缓冲区没锁定则死机
 			panic(DEVICE_NAME ": block not locked"); \
 	}
 
