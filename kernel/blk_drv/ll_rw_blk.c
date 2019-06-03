8aa67ae22071125b1a006bfdf502c01c8ff884c9
pencc <yxpeng@linx-info.com>
2019-06-03 22:43:44 +0800

    添加了blk drv注释

--- kernel/blk_drv/ll_rw_blk.c
+++ kernel/blk_drv/ll_rw_blk.c
@@ -96,10 +96,12 @@ static void add_request(struct blk_dev_struct * dev, struct request * req)
 static void make_request(int major,int rw, struct buffer_head * bh)
 {
 	struct request * req;
-	int rw_ahead;			// 逻辑值用于判断是否为READA或者WRITEA命令
+	int rw_ahead;			// 逻辑值，用于判断是否为READA或者WRITEA命令(预读/写)
 
 /* WRITEA/READA is special case - it is not really needed, so if the */
 /* buffer is locked, we just forget about it, else it's a normal read */
+// 当指定缓冲区正在使用而已被上锁时，就会放弃预读/写请求。否则就作为普通的READ/WRITE命令
+// 进行操作。
 	if ((rw_ahead = (rw == READA || rw == WRITEA))) {
 		if (bh->b_lock)
 			return;
@@ -110,6 +112,15 @@ static void make_request(int major,int rw, struct buffer_head * bh)
 	}
 	if (rw!=READ && rw!=WRITE)
 		panic("Bad block dev command, must be R/W/RA/WA");
+	
+	// 看是否有必要添加请求项。两种情况下不必添加请求项。一是当命令是写。但缓冲区中的数据
+	// 在读入之后并没有被修改过; 二是当命令是读，但缓冲区中的数据已经更新过，即与块设备上
+	// 的完全一样。
+	// 这里首先锁定缓冲区对其检查一下。如果此时缓冲区已被上锁，则当前任务就会睡眠，直到被
+	// 明确地唤醒。如果确实是属于上述两种情况，那么就可以直接解锁缓冲区，并返回。
+	// 这里体现了高速缓冲区的用意，在数据可靠的情况下就无须再执行硬盘操作，而直接使用内存
+	// 中的现有数据。缓冲块的b_dirt标志用于表示缓冲块中的数据是否已经被修改过。b_uptodate
+	// 标志用于表示缓冲块中的数据与块设备上是同步的，即从块设备上读入缓冲块后没有修改过。
 	lock_buffer(bh);
 	if ((rw == WRITE && !bh->b_dirt) || (rw == READ && bh->b_uptodate)) {
 		unlock_buffer(bh);
@@ -118,8 +129,11 @@ static void make_request(int major,int rw, struct buffer_head * bh)
 repeat:
 /* we don't allow the write-requests to fill up the queue completely:
  * we want some room for reads: they take precedence. The last third
- * of the requests are only for reads.
+ * of the requests are only for reads. 请求队列后三分之一的空间仅用于读请求
  */
+	// 从后往前搜索，当请求结构request的设备字段dev值=-1时，表示该项未被占用(空闲)。
+	// 如果没有一项是空闲的（此时请求项数组指针已经搜索越过头部），则查看此次请求是否
+	// 是预读/写，如果是则放弃此次请求操作。否则让本次请求操作先睡眠
 	if (rw == READ)
 		req = request+NR_REQUEST;
 	else
@@ -138,18 +152,25 @@ repeat:
 		goto repeat;
 	}
 /* fill up the request-info, and add it to the queue */
-	req->dev = bh->b_dev;
+	req->dev = bh->b_dev;			// 设备号
 	req->cmd = rw;
-	req->errors=0;
-	req->sector = bh->b_blocknr<<1;
-	req->nr_sectors = 2;
-	req->buffer = bh->b_data;
-	req->waiting = NULL;
-	req->bh = bh;
-	req->next = NULL;
-	add_request(major+blk_dev,req);
+	req->errors=0;					// 操作时产生的错误次数
+	req->sector = bh->b_blocknr<<1;	// 起始扇区。块号转换成扇区号（1块=2扇区）
+	req->nr_sectors = 2;			// 本请求项需要读写的扇区数
+	req->buffer = bh->b_data;		// 请求项缓冲区指针指向需读写的数据缓冲区
+	req->waiting = NULL;			// 任务等待操作执行完成的地方
+	req->bh = bh;					// 缓冲块头指针
+	req->next = NULL;				// 指向下一请求项
+	add_request(major+blk_dev,req);	// 将请求项加入队列中（blk_dev[major]，req）
 }
 
+//// 低层读写数据块函数
+// 该函数是块设备驱动程序与系统其它部分的接口函数。通常在fs/buffer.c程序中被调用。
+// 主要功能是创建块设备读写请求项并插入到指定块设备请求队列中。实际的读写操作则是由
+// 设备的request_fn()函数完成。对于硬盘操作，该函数是do_hd_request(); 对于软盘
+// 操作，该函数是do_fd_request(); 对于虚拟盘则是do_rd_request()。另外，在调用
+// 该函数之前，调用者需要首先把读/写块设备的信息保存在缓冲块头结构中，如设备号、块号。
+// 参数：rw - READ、READA、WRITE或WRITEA是命令; bh - 数据缓冲块头指针。
 void ll_rw_block(int rw, struct buffer_head * bh)
 {
 	unsigned int major;
